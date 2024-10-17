d4b5d073257881b41621287b46a8050ba400df30
Kaique Parente <kaique.parente.game@outlook.com>
2024-10-17 02:28:08 -0300

    [ADD] MUI Library

--- frontend/app/requisicao/page.js
+++ frontend/app/requisicao/page.js
@@ -2,9 +2,10 @@
 
 import styled from "styled-components"
 
+import { Button } from "@mui/material";
+
 export default function Requisicao(){
     return(
-        <>
-        </>
+        <Button variant="contained">Olá Mundo</Button>
     );
 }
