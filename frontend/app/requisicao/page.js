a5478975a78aa9692bfb158e17b720de681e1aee
Kaique Parente <kaique.parente.game@outlook.com>
2024-10-25 17:16:53 -0300

    [ADD] Implementação de botões, input.

--- frontend/app/requisicao/page.js
+++ frontend/app/requisicao/page.js
@@ -11,6 +11,14 @@ import { DatePicker, LocalizationProvider} from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import 'dayjs/locale/en-gb';
+import Navbar from "@/components/sideBar/Navbar";
+
+import Image from "next/image";
+import userImage from "@/images/user2.png";
+import { TextField, InputAdornment } from "@mui/material";
+import BotaoPersonalizado from "@/components/generics/BotaoPersonalizado";
+
+
 
 export default function Requisicao(){
 
@@ -94,90 +102,90 @@ export default function Requisicao(){
     };
 
     return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <button type="button" onClick={handleAddRow}>Adicionar LinhaAAAA</button>
+        <div style={{display:"flex"}}>
+            <Navbar/>
+            <div>
+                <div style={{display:"flex", justifyContent:"center", gap:25, marginTop: 85, marginBottom:50}}>
+                    <Image src={userImage}/>
+                    <h1>Realizar Requisição</h1>
+                </div>
+                <form onSubmit={handleSubmit} style={{display:"flex", flexWrap:"wrap"}}>
+ 
+                    <div>
+                        <TextField
+                            value={name} 
+                            onChange={handleNameChange}  
+                            label="Nome"
+                            error
+                        />
 
-                <ComboBox 
-                    listarItens={listarItensComboBox} 
-                    value={produto} 
-                    inputValue={inputProductValue} 
-                    categoria={categoria}
-                    handleValueChange={handleProductValueChange} 
-                    handleInputValueChange={handleInputProductValueChange}
-                />
+                        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
+                            <DatePicker 
+                                label="Data de Inicio" 
+                                value={dataInicio}
+                                onChange={handleDataInicioChange}
+                                slotProps={{
+                                    textField: {
+                                    helperText: 'DD/MM/YYYY',
+                                    },
+                                }}
+                            />
+                        </LocalizationProvider>
 
-                <ComboBox 
-                    listarItens={testListarItensCategoria} 
-                    value={categoria} 
-                    inputValue={inputCategoriaValue} 
-                    handleValueChange={handleCategoriaValueChange} 
-                    handleInputValueChange={handleInputCategoriaValueChange} 
-                />
-                <label>Nome</label>
-                <input 
-                    type="text" 
-                    value={name} 
-                    onChange={handleNameChange}  
-                />
-                
-                <label>Calorias</label>
-                <input 
-                    type="number" 
-                    value={calories} 
-                    onChange={handleCaloriesChange}  
-                />
+                        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
+                            <DatePicker 
+                                label="Data de Entrega" 
+                                value={dataEntrega}
+                                onChange={handleDataEntregaChange}
+                                slotProps={{
+                                    textField: {
+                                    helperText: 'DD/MM/YYYY',
+                                    },
+                                }}
+                            />
+                        </LocalizationProvider>
+        
+                    </div>
+                    
 
-                <label>Gordura (g)</label>
-                <input 
-                    type="number" 
-                    value={fat} 
-                    onChange={handleFatChange}  
-                />
+                    <div>
+                        <ComboBox 
+                            listarItens={testListarItensCategoria} 
+                            value={categoria} 
+                            inputValue={inputCategoriaValue} 
+                            handleValueChange={handleCategoriaValueChange} 
+                            handleInputValueChange={handleInputCategoriaValueChange} 
+                        />
 
-                <label>Carboidratos (g)</label>
-                <input 
-                    type="number" 
-                    value={carbs} 
-                    onChange={handleCarbsChange}  
-                />
+                        <TextField style={{margin: 0+"px"}}
+                            label="Quantidade"
+                            type="number"
+                            sx={{ m: 1, width: '25ch' }}
+                            slotProps={{
+                                input: {
+                                startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+                                },
+                            }}
+                        />
+                    </div>
+                    
+                    <div>
+                        <ComboBox 
+                            listarItens={listarItensComboBox} 
+                            value={produto} 
+                            inputValue={inputProductValue} 
+                            categoria={categoria}
+                            handleValueChange={handleProductValueChange} 
+                            handleInputValueChange={handleInputProductValueChange}
+                        />
 
-                <label>Proteínas (g)</label>
-                <input 
-                    type="number" 
-                    value={protein} 
-                    onChange={handleProteinChange}  
-                />
+                        <BotaoPersonalizado onClick={handleAddRow} text="Adicionar Linha" color="amarelo"/>
+                    </div>
 
-                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
-                    <DatePicker 
-                        label="Data de Inicio" 
-                        value={dataInicio}
-                        onChange={handleDataInicioChange}
-                        slotProps={{
-                            textField: {
-                              helperText: 'DD/MM/YYYY',
-                            },
-                        }}
-                    />
-                </LocalizationProvider>
-
-                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
-                    <DatePicker 
-                        label="Data de Entrega" 
-                        value={dataEntrega}
-                        onChange={handleDataEntregaChange}
-                        slotProps={{
-                            textField: {
-                              helperText: 'DD/MM/YYYY',
-                            },
-                        }}
-                    />
-                </LocalizationProvider>
-
-                <EnhancedTable tableHeader={tableHeader} rows={rows} onDeleteRow={handleDeleteRow}/>
-                <button type="submit">Submit</button>
-            </form>
-        </>
+                    <EnhancedTable tableHeader={tableHeader} rows={rows} onDeleteRow={handleDeleteRow}/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
     );
 }
