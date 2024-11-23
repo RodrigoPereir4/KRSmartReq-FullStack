d48a0469f2c104e7352160c47125e94ac2c0c60f
RodrigoPereir4 <digolima167@gmail.com>
2024-11-23 00:22:56 -0300

    [UPDATE] Adição de roles, service e autenticador de senha criptografada

--- backend/demo/src/main/java/com/krsoftwares/demo/controllers/UserController.java
+++ backend/demo/src/main/java/com/krsoftwares/demo/controllers/UserController.java
@@ -4,79 +4,64 @@ import java.util.Optional;
 
 import org.hibernate.PropertyValueException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.krsoftwares.demo.models.UserModel;
 import com.krsoftwares.demo.repository.UserRepository;
+import com.krsoftwares.demo.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(origins = "*")
 public class UserController {
 
-    @Autowired 
+    @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @PostMapping("/login")
-    public String loginUsuario(@RequestBody UserModel userModel){
+    public String loginUsuario(@RequestBody UserModel userModel) {
 
         Optional<UserModel> userOptional = userRepository.findByEmail(userModel.getEmail());
-        if(!userOptional.isPresent()){
-            return "Usuário não encontrado!";
+
+        if (!userOptional.isPresent()) {
+            return "Usuário ou senha incorretos.";
         }
-        if(!userOptional.get().getPassword().equals(userModel.getPassword())){
-            return "Senha incorreta";
+
+        UserModel user = userOptional.get();
+        
+        if (!passwordEncoder.matches(userModel.getPassword(), user.getPassword())) {
+            return "Usuário ou senha incorretos.";
         }
         return "Login realizado com sucesso!";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/listar")
-    public Iterable<UserModel> listarUsuarios(){
+    public Iterable<UserModel> listarUsuarios() {
         return userRepository.findAll();
     }
 
-    @GetMapping("/listarPorCategoria")
-    public Iterable<String> listarUsuarios(@RequestParam(required = false) Integer setor){
-        if(setor == null){
-            return userRepository.findAllUserEmail();
-        }
-        return userRepository.findBySetor(setor);
-    }
-
-    @GetMapping("/procurarProdutoNome")
-    public UserModel procurarProdutoNome(@RequestParam(required = true) String nome){
-        Optional<UserModel> userOptional = userRepository.findByEmail(nome);
-        if(userOptional.isPresent()){
-            return userOptional.get();
-        } else {
-            return null;
-        }
-    }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/cadastrar")
-    public String cadastrarUsuario(@RequestBody UserModel user){
-        String msg = "Erro! Não foi possivel cadastrar o usuário";
-        
-        try{
-            if(userRepository.existsByEmail(user.getEmail())){
-                msg = "Erro! Esse email já está cadastrado no sistema";
-            }
-            
-            if(user.getEmail()!=null && user.getPassword()!=null && !userRepository.existsByEmail(user.getEmail())){
-                userRepository.save(user);
-                msg="Usuario Cadastrado com sucesso!";
-            }
-        } catch(PropertyValueException e){
-            msg = "Preencha todos os campos necessarios!";
-        }
-        
-        return msg;
+    public ResponseEntity create(@RequestBody UserModel user) {
+         userService.create(user);
+         return ResponseEntity.ok("Usuário cadastrado!");
     }
 
 }
