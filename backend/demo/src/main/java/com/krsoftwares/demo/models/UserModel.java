c7adc2393b75924c147137e87119b4c7e2c37f6b
RodrigoPereir4 <digolima167@gmail.com>
2024-11-24 00:45:35 -0300

    [UPDATE] Adição do atributo role para definir as permissões do usuário

--- backend/demo/src/main/java/com/krsoftwares/demo/models/UserModel.java
+++ backend/demo/src/main/java/com/krsoftwares/demo/models/UserModel.java
@@ -1,19 +1,34 @@
 package com.krsoftwares.demo.models;
 
+
+import java.util.List;
+
+import com.krsoftwares.demo.role.Role;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+
 @Entity
-@Table(name = "Usuario")
-public class UserModel {
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserModel{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,5 +43,11 @@ public class UserModel {
     @ManyToOne
     @JoinColumn(name = "setor_id", nullable = false)
     private SetorModel setor;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 
 } 
