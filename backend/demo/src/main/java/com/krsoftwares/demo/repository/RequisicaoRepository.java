4159dedb2f7c5994c19bb2ee2f245e249ee07ed3
RodrigoPereir4 <digolima167@gmail.com>
2024-11-28 01:48:52 -0300

    [ADD] Select para trazer a quantidade de requisições vinculadas a cada setor

--- backend/demo/src/main/java/com/krsoftwares/demo/repository/RequisicaoRepository.java
+++ backend/demo/src/main/java/com/krsoftwares/demo/repository/RequisicaoRepository.java
@@ -1,13 +1,24 @@
 package com.krsoftwares.demo.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.krsoftwares.demo.dto.SetorRequisicaoDTO;
 import com.krsoftwares.demo.models.RequisicaoModel;
 
 @Repository
 public interface RequisicaoRepository extends JpaRepository<RequisicaoModel, Long> {
+
+    @Query("SELECT new com.krsoftwares.demo.dto.SetorRequisicaoDTO(s.setorNome, s.setorId, r.status, COUNT(r.requisicaoId)) " +
+    "FROM RequisicaoModel r " +
+    "JOIN r.usuario u " +
+    "JOIN u.setor s " + 
+    "WHERE r.status = true " +
+    "GROUP BY s.setorNome, s.setorId, r.status")
+    List<SetorRequisicaoDTO> findRequisicaoPorSetor();
     
     Optional<RequisicaoModel> findById (Long id);
 }
