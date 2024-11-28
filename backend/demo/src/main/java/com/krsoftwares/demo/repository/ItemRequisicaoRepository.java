9a8c6220323c66ff24fdcb0767f22e5f8b4fbbae
RodrigoPereir4 <digolima167@gmail.com>
2024-11-28 01:50:34 -0300

    [ADD] Select para trazer os itens requisitados de uma requisição

--- backend/demo/src/main/java/com/krsoftwares/demo/repository/ItemRequisicaoRepository.java
+++ backend/demo/src/main/java/com/krsoftwares/demo/repository/ItemRequisicaoRepository.java
@@ -1,6 +1,10 @@
 package com.krsoftwares.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.krsoftwares.demo.models.ItemRequisicaoModel;
@@ -8,4 +12,6 @@ import com.krsoftwares.demo.models.ItemRequisicaoModel;
 @Repository
 public interface ItemRequisicaoRepository extends JpaRepository<ItemRequisicaoModel, Long> {
     
+    @Query("SELECT i FROM ItemRequisicaoModel i WHERE i.requisicaoId.requisicaoId = :requisicaoId")
+    List<ItemRequisicaoModel> findItensRequisicao(@Param("requisicaoId") Long requisicaoId);
 }
